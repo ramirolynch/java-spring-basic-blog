6c583fce1a6c29037046d8abaf9ed5b072f2438d
ramirolynch <ramirolynch@yahoo.com>
2022-06-29 08:35:35 -0500

    display static data

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,32 @@
 package com.pluralsight.blog;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.pluralsight.blog.model.Post;
+import java.util.List;
+import com.pluralsight.blog.data.PostRepository;
+
+
+
 
 @Controller
 public class BlogController {
 	
+	private PostRepository postRepository;
+	
+	public BlogController(PostRepository postRepository) {
+		
+		this.postRepository = postRepository;
+		
+	}
+	
+	
 	@RequestMapping("/")
 	public String listPosts(ModelMap modelMap) {
 		
-		modelMap.put("title", "Blog Post 1");
+		List<Post> allPosts = postRepository.getAllPosts();
+		
+		modelMap.put("posts", allPosts);
 		
 		return "home";
 	}
